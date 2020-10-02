43de8b29d34d04190be476dda3e721d9099a6cb8
Alex <howesalexandra@gmail.com>
2020-10-02 21:12:05 +0200

    Add django testing into the entrypoint.prod.sh script so that the build will only succeed if the tests succeed.

--- humanitarian_feedback/entrypoint.prod.sh
+++ humanitarian_feedback/entrypoint.prod.sh
@@ -11,6 +11,14 @@ then
     echo "PostgreSQL started"
 fi
 
+# Run django tests
+python manage.py test
+if [ $? -ne 0 ]
+then
+  return 1
+fi
+
+# Run migrations and staticfiles
 python manage.py migrate
 python manage.py collectstatic --noinput
 chmod -R 755 /root/humanitarian_feedback/staticfiles
