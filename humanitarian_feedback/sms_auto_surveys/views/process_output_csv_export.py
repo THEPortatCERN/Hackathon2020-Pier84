72113eb8422fbdb4f79b6afd9ec631d2d155ad73
valeriorosssetti <lamagioviale@gmail.com>
2020-10-03 19:18:36 +0200

    csv export v2.1

--- humanitarian_feedback/sms_auto_surveys/views/process_output_csv_export.py
+++ humanitarian_feedback/sms_auto_surveys/views/process_output_csv_export.py
@@ -8,27 +8,16 @@ from sms_auto_surveys.models import Question
 
 def process_output_csv_export(request):
     
-    new_QR = QuestionResponse(response='test 0', call_sid='Fake call_sid 0', phone_number='+417777777', question=Question.objects.last())
-    new_QR.save()
-    new_QR = QuestionResponse(response='test 1', call_sid='Fake call_sid 1', phone_number='+417777777', question=Question.objects.first())
-    new_QR.save()
-    new_QR = QuestionResponse(response='test 2', call_sid='Fake call_sid 2', phone_number='+417777777', question=Question.objects.last())
-    new_QR.save()
-    new_QR = QuestionResponse(response='test 3', call_sid='Fake call_sid 3', phone_number='+417777777', question=Question.objects.first())
-    new_QR.save()
-    
     response = HttpResponse(content_type='text/csv')
     
     writer = csv.writer(response)
     writer.writerow(['response', 'call_sid', 'phone_number', 'question'])
-    print("BUILDING CSV FILE: HEADER ROW")
+    #print("BUILDING CSV FILE: HEADER ROW")
     
     for questresp in QuestionResponse.objects.all():
-    #for questresp in Question.objects.all():
         try:
-            #writer.writerow([questresp.body, questresp.kind, questresp.survey])
             writer.writerow([questresp.response, questresp.call_sid, questresp.phone_number, questresp.question])
-            print("BUILDING CSV FILE: Row written")
+            #print("BUILDING CSV FILE: Row written")
         except:
             print("BUILDING CSV FILE: CSV download couldn't export row.")
             
